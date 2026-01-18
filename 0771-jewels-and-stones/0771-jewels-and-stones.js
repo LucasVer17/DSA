07af477dcb760d6516f7b2d282055e67aa3bf4b4
Lucas Verissimo Goncalves <149521309+LucasVer17@users.noreply.github.com>
2026-01-17 21:48:32 -0300

    Time: 0 ms (100%), Space: 54.5 MB (52.7%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,28 +4,14 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    const hash = new Map()
-    jewels = jewels.split("")
-    stones = stones.split("")
-    let j = 0
-    for(let i = 0; i < jewels.length; i++)
-    {
-        hash.set(jewels[i], 0)
-        console.log(jewels[i])
-    }
+    const jewelSet = new Set(jewels);
+    let count = 0;
 
-    for(stone of stones)
-    {
-        if(hash.has(stone))
-        {
-            hash.set(stone, hash.get(stone) + 1)
+    for (let stone of stones) {
+        if (jewelSet.has(stone)) {
+            count++;
         }
     }
 
-    for(v of hash.values())
-    {
-        j += v
-    }
-
-    return j
+    return count;
 };
