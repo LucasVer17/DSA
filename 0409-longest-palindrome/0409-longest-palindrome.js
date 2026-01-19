2bdc73c04eab5a39162d820d049d97d67698468a
Lucas Verissimo Goncalves <149521309+LucasVer17@users.noreply.github.com>
2026-01-19 15:08:37 -0300

    Time: 2 ms (91.11%), Space: 55.2 MB (73.07%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,35 +3,25 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    const map = new Map()
-    let count = 0
-    let hasOdd = false
-    for(let i = 0; i < s.length; i++)
-    {
-        if(!map.has(s[i]))
-        {
-            map.set(s[i], 1)
-        }
-        else
-        {
-            map.set(s[i], map.get(s[i]) + 1)
+   const m = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        m.set(s[i], (m.get(s[i]) || 0) + 1);
+    }
+
+    let count = 0;
+    let hasOdd = false;
+
+    for (let v of m.values()) {
+        if (v % 2 === 0) {
+            count += v;
+        } else {
+            count += v - 1;
+            hasOdd = true;
         }
     }
 
-    for(let v of map.values())
-    {
-        if(v % 2 == 0)
-        {
-            count += v
-        }
-        else
-        {
-            count += v - 1
-            hasOdd = true
-        }
-    }
+    if (hasOdd) count += 1;
 
-    if(hasOdd) count += 1
-
-    return count
+    return count;
 };
