cbb6d33ef254126019ded29cd2c31e89af029e4e
Lucas Verissimo Goncalves <149521309+LucasVer17@users.noreply.github.com>
2026-01-26 16:31:10 -0300

    Time: 0 ms (100%), Space: 56.2 MB (91.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -19,16 +19,16 @@ var mergeTwoLists = function(list1, list2) {
 
     while(list1 && list2) {
         if(list1.val < list2.val) {
-            curr.next = list1;
+            start.next = list1;
             list1 = list1.next;
         } else {
-            curr.next = list2;
+            start.next = list2;
             list2 = list2.next;
         }
-        curr = curr.next;
+        start = start.next;
     }
-    if(!list1) curr.next = list2;
-    if(!list2) curr.next = list1;
+    if(!list1) start.next = list2;
+    if(!list2) start.next = list1;
 
-    return start.next;
+    return curr.next;
 };
