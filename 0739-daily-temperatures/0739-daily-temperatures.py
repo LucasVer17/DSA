387d802a9cd6ea8d8d8f1d0f196b26a160e03205
Lucas Verissimo Goncalves <149521309+LucasVer17@users.noreply.github.com>
2026-01-27 13:31:30 -0300

    Time: 96 ms (58.57%), Space: 28.2 MB (54.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -8,5 +8,7 @@ class Solution:
                 stack.pop()
             if stack:
                 return_arr[i] = stack[-1] - i
+            else:
+                return_arr[i] = 0
             stack.append(i)
         return return_arr
