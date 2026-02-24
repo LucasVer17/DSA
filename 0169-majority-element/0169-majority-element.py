35c41f9cdfd2b2050b757e100088c922634db01b
Lucas Verissimo Goncalves <lucasverissimo1799@outlook.com>
2026-02-24 15:55:38 -0300

    Time: 7 ms (57.65%), Space: 21.2 MB (40.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
-        total = 0
-        total_return = 0
-        for i in nums:
-            dic[i] = dic.get(i, 0) + 1
+        
         return Counter(nums).most_common(1)[0][0]
