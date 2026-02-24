0c6b6d0b681904ce21379f4e31804510a26c9d15
Lucas Verissimo Goncalves <lucasverissimo1799@outlook.com>
2026-02-24 15:57:44 -0300

    Time: 11 ms (32.45%), Space: 21.4 MB (18.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dic = {}
-        total = 0
-        total_return = 0
+        frequencia_max = 0
+        resultado = nums[0]
         for i in nums:
-            dic[i] = dic.get(i, 0) + 1
-        for k, v in dic.items():
-            if v > total:
-                total = v
-                total_return = k
-        return total_return
+            contagem = dic[i] = dic.get(i, 0) + 1
+            if contagem > frequencia_max:
+                frequencia_max = contagem
+                resultado = i
+        return resultado
