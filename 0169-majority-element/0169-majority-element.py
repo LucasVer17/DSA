39d148e001bce095d82c463f749691d60e3e67d1
Lucas Verissimo Goncalves <lucasverissimo1799@outlook.com>
2026-02-24 15:55:31 -0300

    Time: 15 ms (18.92%), Space: 21.3 MB (40.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,4 +5,4 @@ class Solution:
         total_return = 0
         for i in nums:
             dic[i] = dic.get(i, 0) + 1
-        return max(dic, key=dic.get)
+        return Counter(nums).most_common(1)[0][0]
